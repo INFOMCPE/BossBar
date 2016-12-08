f801f55e0a8dab8f6070b87a12466af03a220789
Aleksey Lozovagin <Oleksih@gmail.com>
2016-12-08 16:15:50 +0200

    Update BossBar.php

--- src/opkiler22789/BossBar.php
+++ src/opkiler22789/BossBar.php
@@ -13,7 +13,10 @@
 	use pocketmine\Player;
 	use pocketmine\command\ConsoleCommandSender; //Консоль КМД
 	use pocketmine\Server;
+	use pocketmine\math\Vector3;
+	use pocketmine\event\player\PlayerMoveEvent;
 	//use _64FF00\PurePerms\PurePerms;
+	use opkiler22789\Epic;
 	use BossBarAPI\API;
 
 	class BossBar extends PluginBase implements Listener {
@@ -34,17 +37,126 @@
    public function update(){
 		    $this->getServer()->getScheduler()->scheduleTask(new UpdaterTask($this, $this->getDescription()->getVersion()));
 	  }
+		
+
 		public function onPlayerJoin(PlayerJoinEvent $event) {
 		    $id = $this->data["id"] ;
 			$BossText = $this->data["$id"]['text']; 
-		    
-		    $this->eid = API::addBossBar([$event->getPlayer()], "§a{$BossText}"); 
+		    $epic = $this->data["$id"]['epic'];
+		if($BossText != null){
+		   $this->eid = API::addBossBar([$event->getPlayer()], "§a{$BossText}"); }
+		if($epic == null){
 		    API::setTitle(sprintf("§e{$BossText}"), $this->eid); 
+		}
+		
+		
 			$per = $this->data["$id"]['per'];
 					if($per != null){
 					API::setPercentage($per, $this->eid); 
 					}
 		}
+		
+		
+		
+		
+		
+		public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+			$cfg = $this->getConfig();
+			switch($command->getName()){
+				
+				case "bb": 
+				if(count($args) == 0){
+					$sender->sendMessage("§9§l—————§4Boss§eBar§9—————\n§6/bb set (id) (text) [%] Установка BossBar\n§6/bb del (id) Удаление BoosBar "); 
+					}
+					switch($args [0]){
+				
+				
+				
+				case "set": 
+				if($sender->hasPermission("bossbar.add")){
+					$args[2] = str_replace("%g", " ", $args[2]); 
+					$epic = $args[4];
+					$per = $args[3];
+					$text = $args[2];
+					$id = $args[1]; 
+					if($text != null){
+					if ($per != null){
+					$this->data["$id"]['per'] = $per; 
+					}else{
+						$this->data["$id"]['per'] = 0; 
+						}
+					$this->data["$id"]['text'] = $text; 
+					
+					$this->data["id"] = $id; 
+					
+					$BossText = $this->data["$id"]['text']; 
+					$geto = Server::getInstance()->getOnlinePlayers();
+					$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
+					
+					API::setTitle(sprintf("§e{$BossText}"), $this->eid); 
+					$per = $this->data["$id"]['per'];
+					if($per != null){
+					API::setPercentage($per, $this->eid); 
+					}
+					$sender->sendMessage("[§4Boss§eBar] Текст: {$text} выставлен на id: {$id}"); 
+					}else{
+						$sender->sendMessage("[§4Boss§eBar]Ты не указал(а) текст"); 
+						}
+						}else{
+							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
+							}
+					return true;
+					
+					case "del": 
+					if($sender->hasPermission("bossbar.del")){
+					$id = $args[1]; 
+					
+					$BossText = $this->data["$id"]['text'];
+					if ($BossText != null){
+                    
+					$geto = Server::getInstance()->getOnlinePlayers();
+					//$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
+					API::removeBossBar($geto, $this->eid);
+					API::removeBossBar([$sender->getPlayer()], $this->eid);
+					$this->data["$id"] = null; 
+					$sender->sendMessage("[§4Boss§eBar] Успешно: id: {$id} удалено"); 
+					}else{
+						$sender->sendMessage("[§4Boss§eBar]§4Ошибка id введен  не верно"); 
+						}
+						}else{
+							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
+							}
+							case "epic": 
+					if($sender->hasPermission("bossbar.epic")){
+					
+						}else{
+							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
+							}
+					
+					
+					return true;
+					
+				}
+			}
+		}
+		
+		
+	
+		public function save(){
+			$cfg = new Config($this->getDataFolder().'data.yml', Config::YAML);
+			$cfg->setAll($this->data);
+			$cfg->save();
+		}
+		
+		public function onDisable()
+		{
+			$cfg = new Config($this->getDataFolder().'data.yml', Config::YAML);
+			$cfg->setAll($this->data);
+			$cfg->save();
+		}	
+	}
+	
+?>
 		
 		
 		
