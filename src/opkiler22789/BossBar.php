a56ff9b0b76518bc9f1c29326fb40bd00120558c
Aleksey Lozovagin <Oleksih@gmail.com>
2017-01-07 09:19:33 +0200

    UPDATE!

--- src/opkiler22789/BossBar.php
+++ src/opkiler22789/BossBar.php
@@ -40,17 +40,17 @@
 		
 
 		public function onPlayerJoin(PlayerJoinEvent $event) {
+			error_reporting(0);
 		    $id = $this->data["id"] ;
-			$BossText = $this->data["$id"]['text']; 
-		    $epic = $this->data["$id"]['epic'];
+			$BossText = $this->data['bossbars']["$id"]['text']; 
+		    
 		if($BossText != null){
-		   $this->eid = API::addBossBar([$event->getPlayer()], "§a{$BossText}"); }
-		if($epic == null){
+		   $this->eid = API::addBossBar([$event->getPlayer()], "§a{$BossText}"); 
 		    API::setTitle(sprintf("§e{$BossText}"), $this->eid); 
 		}
 		
 		
-			$per = $this->data["$id"]['per'];
+			$per = $this->data['bossbars']["$id"]['per'];
 					if($per != null){
 					API::setPercentage($per, $this->eid); 
 					}
@@ -66,7 +66,7 @@
 				
 				case "bb": 
 				if(count($args) == 0){
-					$sender->sendMessage("§9§l—————§4Boss§eBar§9—————\n§6/bb set (id) (text) [%] Установка BossBar\n§6/bb del (id) Удаление BoosBar "); 
+					$sender->sendMessage("§9§l—————§4Boss§eBar§9—————\n§6/bb set (id) (text) [%] Установка BossBar\n§6/bb del (id) Удаление BoosBar \n§6 /bb list - Получить список BossBar'ов \n §6/bb def (id) - установить id по умолчанию"); 
 					}
 					switch($args [0]){
 				
@@ -81,20 +81,19 @@
 					$id = $args[1]; 
 					if($text != null){
 					if ($per != null){
-					$this->data["$id"]['per'] = $per; 
+					$this->data['bossbars']["$id"]['per'] = $per; 
 					}else{
-						$this->data["$id"]['per'] = 0; 
+						$this->data['bossbars']["$id"]['per'] = 0; 
 						}
-					$this->data["$id"]['text'] = $text; 
-					
+					$this->data['bossbars']["$id"]['text'] = $text; 
 					$this->data["id"] = $id; 
 					
-					$BossText = $this->data["$id"]['text']; 
+					$BossText = $this->data['bossbars']["$id"]['text']; 
 					$geto = Server::getInstance()->getOnlinePlayers();
 					$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
 					
 					API::setTitle(sprintf("§e{$BossText}"), $this->eid); 
-					$per = $this->data["$id"]['per'];
+					$per = $this->data['bossbars']["$id"]['per'];
 					if($per != null){
 					API::setPercentage($per, $this->eid); 
 					}
@@ -111,14 +110,14 @@
 					if($sender->hasPermission("bossbar.del")){
 					$id = $args[1]; 
 					
-					$BossText = $this->data["$id"]['text'];
+					$BossText = $this->data['bossbars']["$id"]['text'];
 					if ($BossText != null){
                     
 					$geto = Server::getInstance()->getOnlinePlayers();
 					//$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
 					API::removeBossBar($geto, $this->eid);
 					API::removeBossBar([$sender->getPlayer()], $this->eid);
-					$this->data["$id"] = null; 
+					$this->data["bossbars"]["$id"] = null; 
 					$sender->sendMessage("[§4Boss§eBar] Успешно: id: {$id} удалено"); 
 					}else{
 						$sender->sendMessage("[§4Boss§eBar]§4Ошибка id введен  не верно"); 
@@ -126,116 +125,33 @@
 						}else{
 							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
 							}
-							case "epic": 
-					if($sender->hasPermission("bossbar.epic")){
-					
-						}else{
-							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
-							}
+							
 					
 					
 					return true;
-					
+					case "list":
+					if($sender->hasPermission("bossbar.list")){
+					arsort($this->data["bossbars"] );
+					foreach ($this->data["bossbars"] as $id => $data) {
+						$sender->sendMessage("ID: {$id} Текст: {$data['text']} Заполнено на {$data['per']} %");
+						}
+						}else{
+							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
+							}
+					return true;
+					case "def":
+					if($sender->hasPermission("bossbar.def")){
+					$this->data['id'] = $args[1];
+					}else{
+						$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
+						}
+					return true;
 				}
 			}
 		}
 		
 		
 	
-		public function save(){
-			$cfg = new Config($this->getDataFolder().'data.yml', Config::YAML);
-			$cfg->setAll($this->data);
-			$cfg->save();
-		}
-		
-		public function onDisable()
-		{
-			$cfg = new Config($this->getDataFolder().'data.yml', Config::YAML);
-			$cfg->setAll($this->data);
-			$cfg->save();
-		}	
-	}
-	
-?>
-		
-		
-		
-		
-		
-		public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-			$cfg = $this->getConfig();
-			switch($command->getName()){
-				
-				case "bb": 
-				if(count($args) == 0){
-					$sender->sendMessage("§9§l—————§4Boss§eBar§9—————\n§6/bb set (id) (text) [%] Установка BossBar\n§6/bb del (id) Удаление BoosBar "); 
-					}
-					switch($args [0]){
-				
-				
-				
-				case "set": 
-				if($sender->hasPermission("bossbar.add")){
-					$args[2] = str_replace("%g", " ", $args[2]); 
-					$per = $args[3];
-					$text = $args[2];
-					$id = $args[1]; 
-					if($text != null){
-					if ($per != null){
-					$this->data["$id"]['per'] = $per; 
-					}else{
-						$this->data["$id"]['per'] = 0; 
-						}
-					$this->data["$id"]['text'] = $text; 
-					$this->data["id"] = $id; 
-					
-					$BossText = $this->data["$id"]['text']; 
-					$geto = Server::getInstance()->getOnlinePlayers();
-					$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
-					
-					API::setTitle(sprintf("§e{$BossText}"), $this->eid); 
-					$per = $this->data["$id"]['per'];
-					if($per != null){
-					API::setPercentage($per, $this->eid); 
-					}
-					$sender->sendMessage("[§4Boss§eBar] Текст: {$text} выставлен на id: {$id}"); 
-					}else{
-						$sender->sendMessage("[§4Boss§eBar]Ты не указал(а) текст"); 
-						}
-						}else{
-							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
-							}
-					return true;
-					
-					case "del": 
-					if($sender->hasPermission("bossbar.del")){
-					$id = $args[1]; 
-					
-					$BossText = $this->data["$id"]['text'];
-					if ($BossText != null){
-                    
-					$geto = Server::getInstance()->getOnlinePlayers();
-					//$this->eid = API::addBossBar($geto, "§a{$BossText}"); 
-					API::removeBossBar($geto, $this->eid);
-					API::removeBossBar([$sender->getPlayer()], $this->eid);
-					$this->data["$id"] = null; 
-					$sender->sendMessage("[§4Boss§eBar] Успешно: id: {$id} удалено"); 
-					}else{
-						$sender->sendMessage("[§4Boss§eBar]§4Ошибка id введен  не верно"); 
-						}
-						}else{
-							$sender->sendMessage("[§4Boss§eBar]Недостаточно прав"); 
-							}
-					
-					
-					return true;
-					
-				}
-			}
-		}
-		
-		
-		
 		public function save(){
 			$cfg = new Config($this->getDataFolder().'data.yml', Config::YAML);
 			$cfg->setAll($this->data);
